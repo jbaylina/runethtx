df96fa8bef3b338aaf7d749dd015e913a5b1d724
Jordi Baylina <jordi@baylina.cat>
2017-02-10 07:22:22 +0100

    Deploy added

--- dist/runethtx.js
+++ dist/runethtx.js
@@ -12,13 +12,132 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-exports.default = runEthTx;
+exports.default = {
+    send: send,
+    deploy: deploy
+};
 
 
-function runEthTx(_ref, cb) {
-    var contract = _ref.contract,
-        method = _ref.method,
-        opts = _objectWithoutProperties(_ref, ["contract", "method"]);
+function deploy(web3, _ref, cb) {
+    var abi = _ref.abi,
+        byteCode = _ref.byteCode,
+        opts = _objectWithoutProperties(_ref, ["abi", "byteCode"]);
+
+    var promise = new Promise(function (resolve, reject) {
+        var constructorAbi = abi.find(function (_ref2) {
+            var type = _ref2.type;
+            return type === "constructor";
+        });
+        var paramNames = void 0;
+        var contract = void 0;
+        var fromAccount = void 0;
+        var gas = void 0;
+        if (constructorAbi) {
+            paramNames = constructorAbi.inputs.map(function (_ref3) {
+                var name = _ref3.name;
+
+                if (name[0] === "_") {
+                    return name.substring(1);
+                }
+                return name;
+            });
+        } else {
+            paramNames = [];
+        }
+        _async2.default.series([function (cb1) {
+            if (opts.from) {
+                fromAccount = opts.from;
+                setTimeout(cb1, 1);
+            } else {
+                web3.eth.getAccounts(function (err, _accounts) {
+                    if (err) {
+                        console.log("xxxxx -> " + err);
+                        cb1(err);
+                        return;
+                    }
+                    if (_accounts.length === 0) {
+                        cb1(new Error("No account to deploy a contract"));
+                        return;
+                    }
+                    fromAccount = _accounts[0];
+                    cb1();
+                });
+            }
+        }, function (cb2) {
+            var params = paramNames.map(function (name) {
+                return opts[name];
+            });
+            if (opts.verbose) console.log("constructor: " + JSON.stringify(params));
+            params.push({
+                from: fromAccount,
+                value: opts.value || 0,
+                data: byteCode,
+                gas: 4000000
+            });
+            var data = web3.eth.contract(abi).new.getData.apply(null, params);
+
+            web3.eth.estimateGas({
+                from: fromAccount,
+                value: opts.value || 0,
+                data: data,
+                gas: 4000000
+            }, function (err, _gas) {
+                if (err) {
+                    cb2(err);
+                } else if (_gas >= 4000000) {
+                    cb2(new Error("throw"));
+                } else {
+                    gas = _gas;
+                    gas += opts.extraGas ? opts.extraGas : 10000;
+                    cb2();
+                }
+            });
+        }, function (cb3) {
+            var params = paramNames.map(function (name) {
+                return opts[name];
+            });
+            params.push({
+                from: fromAccount,
+                value: opts.value || 0,
+                data: byteCode,
+                gas: gas
+            });
+            params.push(function (err, _contract) {
+                if (err) {
+                    cb3(err);
+                    return;
+                }
+                if (typeof _contract.address !== "undefined") {
+                    contract = _contract;
+                    cb3();
+                }
+            });
+            var ctr = web3.eth.contract(abi);
+            ctr.new.apply(ctr, params);
+        }], function (err2) {
+            if (err2) {
+                reject(err2);
+            } else {
+                resolve(contract);
+            }
+        });
+    });
+
+    if (cb) {
+        promise.then(function (value) {
+            cb(null, value);
+        }, function (reason) {
+            cb(reason);
+        });
+    } else {
+        return promise;
+    }
+}
+
+function send(_ref4, cb) {
+    var contract = _ref4.contract,
+        method = _ref4.method,
+        opts = _objectWithoutProperties(_ref4, ["contract", "method"]);
 
     var promise = new Promise(function (resolve, reject) {
         if (!contract) {
@@ -32,9 +151,11 @@ function runEthTx(_ref, cb) {
             return;
         }
 
-        var methodAbi = contract.abi.find(function (_ref2) {
-            var name = _ref2.name,
-                inputs = _ref2.inputs;
+        var errRes = void 0;
+
+        var methodAbi = contract.abi.find(function (_ref5) {
+            var name = _ref5.name,
+                inputs = _ref5.inputs;
 
             if (name !== method) return false;
             var paramNames = inputs.map(function (param) {
@@ -44,18 +165,26 @@ function runEthTx(_ref, cb) {
                 return param.name;
             });
             for (var i = 0; i < paramNames.length; i += 1) {
-                if (typeof opts[paramNames[i]] === "undefined") return false;
+                if (typeof opts[paramNames[i]] === "undefined") {
+                    errRes = new Error("Param " + paramNames[i] + " not found.");
+                    return false;
+                }
             }
             return true;
         });
+
+        if (errRes) {
+            reject(errRes);
+            return;
+        }
 
         if (!methodAbi) {
             reject(new Error("Invalid method"));
             return;
         }
 
-        var paramNames = methodAbi.inputs.map(function (_ref3) {
-            var name = _ref3.name;
+        var paramNames = methodAbi.inputs.map(function (_ref6) {
+            var name = _ref6.name;
 
             if (name[0] === "_") {
                 return name.substring(1);
@@ -91,8 +220,10 @@ function runEthTx(_ref, cb) {
             var params = paramNames.map(function (name) {
                 return opts[name];
             });
+            if (opts.verbose) console.log(method + ": " + JSON.stringify(params));
             params.push({
                 from: fromAccount,
+                value: opts.value,
                 gas: 4000000
             });
             params.push(function (err, _gas) {
@@ -112,9 +243,9 @@ function runEthTx(_ref, cb) {
             var params = paramNames.map(function (name) {
                 return opts[name];
             });
-            //                console.log(method + ": " + JSON.stringify(params));
             params.push({
                 from: fromAccount,
+                value: opts.value,
                 gas: gas
             });
             params.push(function (err, _txHash) {
@@ -127,9 +258,12 @@ function runEthTx(_ref, cb) {
             });
 
             contract[method].apply(null, params);
-        }], function (err) {
-            if (err) {
-                reject(err);
+        }, function (cb4) {
+            // eslint-disable-next-line no-underscore-dangle
+            contract._eth.getTransactionReceipt(txHash, cb4);
+        }], function (err2) {
+            if (err2) {
+                reject(err2);
             } else {
                 resolve(txHash);
             }
--- js/runethtx.js
+++ js/runethtx.js
@@ -1,8 +1,120 @@
 import async from "async";
 
-export default runEthTx;
+export default {
+    send,
+    deploy,
+};
 
-function runEthTx({ contract, method, ...opts }, cb) {
+function deploy(web3, { abi, byteCode, ...opts }, cb) {
+    const promise = new Promise((resolve, reject) => {
+        const constructorAbi = abi.find(({ type }) => (type === "constructor"));
+        let paramNames;
+        let contract;
+        let fromAccount;
+        let gas;
+        if (constructorAbi) {
+            paramNames = constructorAbi.inputs.map(({ name }) => {
+                if (name[ 0 ] === "_") {
+                    return name.substring(1);
+                }
+                return name;
+            });
+        } else {
+            paramNames = [];
+        }
+        async.series([
+            (cb1) => {
+                if (opts.from) {
+                    fromAccount = opts.from;
+                    setTimeout(cb1, 1);
+                } else {
+                    web3.eth.getAccounts((err, _accounts) => {
+                        if (err) {
+                            console.log("xxxxx -> " + err);
+                            cb1(err);
+                            return;
+                        }
+                        if (_accounts.length === 0) {
+                            cb1(new Error("No account to deploy a contract"));
+                            return;
+                        }
+                        fromAccount = _accounts[ 0 ];
+                        cb1();
+                    });
+                }
+            },
+            (cb2) => {
+                const params = paramNames.map(name => opts[ name ]);
+                if (opts.verbose) console.log("constructor: " + JSON.stringify(params));
+                params.push({
+                    from: fromAccount,
+                    value: opts.value || 0,
+                    data: byteCode,
+                    gas: 4000000,
+                });
+                const data = web3.eth.contract(abi).new.getData.apply(null, params);
+
+                web3.eth.estimateGas({
+                    from: fromAccount,
+                    value: opts.value || 0,
+                    data,
+                    gas: 4000000,
+                }, (err, _gas) => {
+                    if (err) {
+                        cb2(err);
+                    } else if (_gas >= 4000000) {
+                        cb2(new Error("throw"));
+                    } else {
+                        gas = _gas;
+                        gas += opts.extraGas ? opts.extraGas : 10000;
+                        cb2();
+                    }
+                });
+            },
+            (cb3) => {
+                const params = paramNames.map(name => opts[ name ]);
+                params.push({
+                    from: fromAccount,
+                    value: opts.value || 0,
+                    data: byteCode,
+                    gas,
+                });
+                params.push((err, _contract) => {
+                    if (err) {
+                        cb3(err);
+                        return;
+                    }
+                    if (typeof _contract.address !== "undefined") {
+                        contract = _contract;
+                        cb3();
+                    }
+                });
+                const ctr = web3.eth.contract(abi);
+                ctr.new.apply(ctr, params);
+            },
+        ], (err2) => {
+            if (err2) {
+                reject(err2);
+            } else {
+                resolve(contract);
+            }
+        });
+    });
+
+    if (cb) {
+        promise.then(
+            (value) => {
+                cb(null, value);
+            },
+            (reason) => {
+                cb(reason);
+            });
+    } else {
+        return promise;
+    }
+}
+
+function send({ contract, method, ...opts }, cb) {
     const promise = new Promise((resolve, reject) => {
         if (!contract) {
             reject(new Error("Contract not defined"));
@@ -15,6 +127,8 @@ function runEthTx({ contract, method, ...opts }, cb) {
             return;
         }
 
+        let errRes;
+
         const methodAbi = contract.abi.find(({ name, inputs }) => {
             if (name !== method) return false;
             const paramNames = inputs.map((param) => {
@@ -24,10 +138,18 @@ function runEthTx({ contract, method, ...opts }, cb) {
                 return param.name;
             });
             for (let i = 0; i < paramNames.length; i += 1) {
-                if (typeof opts[ paramNames[ i ] ] === "undefined") return false;
+                if (typeof opts[ paramNames[ i ] ] === "undefined") {
+                    errRes = new Error("Param " + paramNames[ i ] + " not found.");
+                    return false;
+                }
             }
             return true;
         });
+
+        if (errRes) {
+            reject(errRes);
+            return;
+        }
 
         if (!methodAbi) {
             reject(new Error("Invalid method"));
@@ -69,8 +191,10 @@ function runEthTx({ contract, method, ...opts }, cb) {
             },
             (cb2) => {
                 const params = paramNames.map(name => opts[ name ]);
+                if (opts.verbose) console.log(method + ": " + JSON.stringify(params));
                 params.push({
                     from: fromAccount,
+                    value: opts.value,
                     gas: 4000000,
                 });
                 params.push((err, _gas) => {
@@ -89,9 +213,9 @@ function runEthTx({ contract, method, ...opts }, cb) {
             },
             (cb3) => {
                 const params = paramNames.map(name => opts[ name ]);
-//                console.log(method + ": " + JSON.stringify(params));
                 params.push({
                     from: fromAccount,
+                    value: opts.value,
                     gas,
                 });
                 params.push((err, _txHash) => {
@@ -105,9 +229,13 @@ function runEthTx({ contract, method, ...opts }, cb) {
 
                 contract[ method ].apply(null, params);
             },
-        ], (err) => {
-            if (err) {
-                reject(err);
+            (cb4) => {
+                // eslint-disable-next-line no-underscore-dangle
+                contract._eth.getTransactionReceipt(txHash, cb4);
+            },
+        ], (err2) => {
+            if (err2) {
+                reject(err2);
             } else {
                 resolve(txHash);
             }
